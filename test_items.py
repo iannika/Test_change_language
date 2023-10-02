026bfbbbe1d0bff6ea528a1f03e2c86b79dd2f94
iannika <iannika7@gmail.com>
2023-10-02 11:33:23 +0300

    edit assert

--- test_items.py
+++ test_items.py
@@ -1,15 +1,17 @@
 import pytest
 from selenium.webdriver.common.by import By
 
-link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-# link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-cathedral-the-bazaar_190/"
+# link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-cathedral-the-bazaar_190/"
+
 
 
 def test_guest_should_add_to_basket(browser):
     browser.get(link)
-    button = browser.find_element(By.CSS_SELECTOR, "#add_to_basket_form > button")  
-    assert button != [], 'Кнопка не найдена!'
-
-
-
+    try: 
+        button = browser.find_element(By.CSS_SELECTOR, "#add_to_basket_form > button")
+        button = True
+    except:
+        button = False  
+        assert button, "button not found"
 
